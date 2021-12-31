f809c43556cdc65732e977ab0d86ee75093891c8
Tim Dorr <git@timdorr.com>
2021-12-30 20:54:35 -0500

    Add a default title

--- app/root.tsx
+++ app/root.tsx
@@ -9,7 +9,7 @@ import {
 import type { MetaFunction } from 'remix'
 
 export const meta: MetaFunction = () => {
-  return { title: 'New Remix App' }
+  return { title: 'Remotion' }
 }
 
 export default function App() {
